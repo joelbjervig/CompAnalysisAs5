f9f296d61675f0a0ebcfd01e58d390e2038519ac
Joel Bjervig <joelfbjervig@gmail.com>
2021-01-17 13:33:35 +0100

    cell2mat

--- my_classifier_joel.m
+++ my_classifier_joel.m
@@ -8,25 +8,56 @@ function S = my_classifier_joel(im, parameters1, parameter2)
 %on average, approx. a 4% chance of guessing the correct answer. 
 %
 
-%load data
-labels = importdata("labels.txt");
-S = floor(rand(1,3)*3);
+%example to use the evaluate_classifier.m file, the output should be of
+%this form: [2,1,3] i.e an array of length 3 containg the classified digits
+
+
+%load labels in cell format to match imds
+labels = mat2cell(importdata("labels.txt"));
 
 % image datastore size of one image: 301*225 pixels
 imds = imageDatastore('imagedata');
+
+% add labels to image datastore
+imds.Labels = labels;
+
 %size of training images
 [imSizeX, imSizeY] = size(readimage(imds,1));
 
 % build layers
 layers = [
-    imageInputLayer([imSizeX,imSizeY]);
+    
+    % imput layer of the same size of the training images
+    imageInputLayer([imSizeX,imSizeY],'name','Input layer');
+    
     % create 32 convolution filters of size 3*3
-    convolution2dLayer(3,32)
-    reluLayer
-    maxPooling2dLayer(2,'Stride',2)
-    fullyConnectedLayer(10)
-    softmaxLayer
-    classificationLayer];
+    convolution2dLayer(3,32,'name','Convolution layers')
+
+    % rectified linear activation function
+    %  - output the input if it is positive, otherwise, output is zero
+    reluLayer('name','ReLU')
+    
+    % maxpooling (size of ) mdownsamples the input to help over-fitting
+    % by providing an abstracted form of the representation
+    maxPooling2dLayer(2,'Stride',2,'name','Max pooling')
+    
+    % Fully Connected (output size = 10) layers in a neural networks are those layers
+    % where all the inputs from one layer are connected to every
+    % activation unit of the next layer
+    fullyConnectedLayer(10, 'name','Fully connected layer')
+    
+    % Softmax assigns decimal probabilities to each class in a multi-class problem
+    softmaxLayer('name','Softmax')
+    % classification 
+    classificationLayer('name','Classification layer')
+    ];
+
+    lgraph = layerGraph(layers);
+    figure
+    plot(lgraph)
+
+    options = trainingOptions('sgdm','MaxEpochs',20,'InitialLearnRate',1e-4,'Verbose',false,'Plots','training-progress');
+    net = trainNetwork(imds,layers,options);
+
 end
-options = 
 
